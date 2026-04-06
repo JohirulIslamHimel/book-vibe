6dc2bae40543219a9c671783cc64ffeac71e1e29
Johirul Islam Himel <himel1199@gmail.com>
2026-04-06 19:04:27 +0600

    Load books data and show all the books

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../component/homepage/Banner";
+import AllBooks from "../../component/homepage/AllBooks";
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
+      <AllBooks></AllBooks>
     </div>
   );
 };
